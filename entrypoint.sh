fc6ce974ebe0b60fc9b2c8f2d3b8c61920edf695
compilercomplied <8614035+compilercomplied@users.noreply.github.com>
2026-02-08 21:29:15 +0100

    validate pulumi passphrase

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,12 @@ if [ -z "$GITHUB_TOKEN" ]; then
     exit 1
 fi
 
+if [ -z "$PULUMI_CONFIG_PASSPHRASE" ]; then
+    echo "Error: PULUMI_CONFIG_PASSPHRASE environment variable is not set."
+    exit 1
+fi
+
+
 # 2. Configure Git Credentials (HTTPS)
 git config --global url."https://${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
 
